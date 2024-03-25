3b14b7d670d6a63aa59e523e5734e5e5dc4b1545
mariya <mariya.yandex.by>
2024-03-25 22:03:13 +0300

    new

--- py95_shop/catalog/admin.py
+++ py95_shop/catalog/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from catalog.models import Seller,Category,Discount,Promocode,Product,Order,CashBack,ProductImage
 
-# Register your models here.
+class ProductAdmin(admin.ModelAdmin):
+    list_display=("article","name","price")
+    search_fields = ("article","name","category__name")
+
+
+admin.site.register(Product, ProductAdmin)
+admin.site.register(Seller)
+admin.site.register(Category)
+admin.site.register(Discount)
+admin.site.register(Promocode)
+admin.site.register(Order)
+admin.site.register(ProductImage)
+admin.site.register(CashBack)
--- py95_shop/users/models.py
+++ py95_shop/users/models.py
@@ -1,30 +1,30 @@
 from django.db import models
-from django.contrib.auth.models import AbstractBaseUser,BaseUserManager,PermissionsMixin
+from django.contrib.auth.models import AbstractBaseUser, BaseUserManager, PermissionsMixin
+
 
 class CustomUserManager(BaseUserManager):
 
-    def create_user(self,email,password,**extra_fields):
+    def create_user(self, email, password, **extra_fields):
         if not email:
             raise ValueError("email must exists")
         if not password:
             raise ValueError('Password field must be fill in ')
-        email=self.normalize_email(email)
-        user=self.model(email=email, **extra_fields)
+        email = self.normalize_email(email)
+        user = self.model(email=email, **extra_fields)
         user.set_password(password)
         user.save()
         return user
 
+    def create_superuser(self, email, password, **extra_fields):
+        extra_fields.setdefault("is_staff", True)
+        extra_fields.setdefault("is_superuser", True)
 
-    def create_superuser(self,email,password,**extra_fields):
-         extra_fields.setdefault("is_staff",True)
-         extra_fields.setdefault("is_superuser",True)
-
-         return self.create_user(email,password,**extra_fields)
-
+        return self.create_user(email, password, **extra_fields)
 
 
 
-class CustomUSer(AbstractBaseUser,PermissionsMixin):
+
+class CustomUser(AbstractBaseUser,PermissionsMixin):
     email=models.EmailField(unique=True)
     phone_number=models.CharField(max_length=14,blank=True,null=True)
     is_active=models.BooleanField(default=False)
